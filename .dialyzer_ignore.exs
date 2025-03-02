85cc67324d5f645d1620e859d99daded1afefdeb
John Downey <jdowney@gmail.com>
2025-03-01 20:12:21 -0600

    Add additional dialyzer ignore for older OTPs

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,6 @@
 [
   {"lib/x509/certificate.ex", "Unknown type: X509.ASN1.record/1."},
   {"lib/x509/csr.ex", "Unknown type: X509.ASN1.record/1."},
-  {"lib/x509/private_key.ex", "Unknown type: :public_key.ec_private_key/0."}
+  {"lib/x509/private_key.ex", "Unknown type: :public_key.ec_private_key/0."},
+  {"lib/x509/private_key.ex", "Unknown type: :public_key.rsa_private_key/0."}
 ]
