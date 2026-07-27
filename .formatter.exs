c85da16ff23688d89bcd6a66bd788de3c3a86623
John Downey <jdowney@gmail.com>
2026-07-26 20:41:25 -0500

    style: pin Styler to the supported Elixir floor

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 [
   import_deps: [:assert_eventually],
   inputs: ["*.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  plugins: [Styler]
+  plugins: [Styler],
+  styler: [minimum_supported_elixir_version: "1.18.0"]
 ]
